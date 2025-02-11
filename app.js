951d52256fbb6a1f8124f37a8b626e7500b56e95
Taha Hussain <thussain170305@gmail.com>
2025-02-11 09:20:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+
+// 1.
+
+// let x = 5, y = 10, z = 15;
+
+// document.write (x,y,z)
+
+// 2.
+
+// 5 legal names
+
+
+// var abc
+// var $ac
+// var _ab
+// var abc123
+// var a12
+
+// 5 illegal names
+
+// var 12
+// var *abc
+// var @$ac
+// var -123
+
+
+// 3(b).
+
+// letters
+// $
+// _
+// Digits (0-9), but not as the first character
+
+// 3(c)
+
+// variable must begin with
+
+// A letter (A-Z, a-z)
+// An underscore (_)
+// A dollar sign ($)
+
+// 3(d)
+
+// varibles are case sensitive
+
+
+// 3(e)
+
+// Variable names should not be JS KEYWORDS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
